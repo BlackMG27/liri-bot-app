d90fd015497180b0e6fe50ed0ee798833b842393
BlackMG27 <oike279@gmail.com>
2019-03-24 11:00:47 -0500

    got the base of the spotifyThisSong function to work

--- liri.js
+++ liri.js
@@ -1,5 +1,4 @@
-//[dotenv]
-//get the variables 
+//get the variables
 require('dotenv').config();
 var Spotify = require('node-spotify-api');
 var keys = require('./keys');
@@ -7,9 +6,12 @@ var spotify = new Spotify(keys.spotify);
 var axios = require('axios');
 var moment = require('moment');
 var fs = require('fs');
-//get the user input 
+//get the user input
 var liriCommand = process.argv[2];
-var liriChoice = process.argv.slice(3).join(" ");
+var liriChoice = process
+    .argv
+    .slice(3)
+    .join(" ");
 
 //switch statement for liriCommands
 switch (liriCommand) {
@@ -28,34 +30,39 @@ switch (liriCommand) {
 }
 //spotify function
 function spotifyThisSong() {
-    //searches spotify 
-    spotify.search({
-        type: 'track',
-        query: liriChoice
-    }, function (err, data) {
-        //catches the error
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        //gets the specific data 
-        console.log(data);
-        console.log('--------------------------------')
-        console.log(data.tracks.items[0]);
-        console.log('--------------------------------')
-        console.log(data.tracks.items[0].album.artists[0].name);
-        console.log(data.tracks.items[0].album.name);
-        //console.log(data.tracks.items[0].album.name);
-        console.log(data.tracks.items[0].album.duration_ms);
-    });
+    //searches spotify
+    spotify
+        .search({
+            type: 'track',
+            query: liriChoice
+        }, function (err, data) {
+            //catches the error
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            //gets the specific data
+
+            //console.log(data.tracks.items[0]);
+            for (song in data.tracks.items) {
+                console.log('----------------------------------------------------------');
+                console.log(`Artist Name: ${data.tracks.items[song].album.artists[0].name}`);
+                console.log(`Album Name: ${data.tracks.items[song].album.name}`);
+                console.log(`Song Name: ${data.tracks.items[song].name}`);
+                console.log(`Song Preview: ${data.tracks.items[song].preview_url}`);
+                console.log('----------------------------------------------------------');
+            }
+
+        });
 }
 
 function movieThis() {
-    //formats liriChoice 
+    //formats liriChoice
     liriChoice = liriChoice.replace(/ /g, '+');
     //puts liriChoice in the movie URL
     var movieURL = `https://www.omdbapi.com/?t=${liriChoice}&y=&plot=short&apikey=trilogy`;
     //calls axios to get the api data
-    axios.get(movieURL)
+    axios
+        .get(movieURL)
         .then(function (response) {
             //gets the movie info
             console.log(`Movie Title: ${response.data.Title}`);
@@ -65,6 +72,7 @@ function movieThis() {
             console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
             console.log(`Movie Language: ${response.data.Language}`);
             console.log(`Movie Actor(s): ${response.data.Actors}`);
+            console.log(`Movie Genre(s): ${response.data.Genre}`);
             console.log(`Movie Plot: ${response.data.Plot}`);
         })
         .catch(function (error) {
@@ -73,18 +81,19 @@ function movieThis() {
 }
 
 function concertThis() {
-    console.log(liriChoice);
-    //gets rid of the white spaces 
+    //console.log(liriChoice);
+    //gets rid of the white spaces
     liriChoice = liriChoice.replace(/ /g, '%20');
     //sets up the api call
     var concertURL = `https://rest.bandsintown.com/artists/${liriChoice}/events?app_id=e0fa108c-68a4-472d-90fc-7da6bc210785`;
-    console.log(concertURL);
+    //console.log(concertURL);
     //gets the ajax calls
     axios.get(concertURL)
-        //smacks the api and spits out the information 
+        //smacks the api and spits out the information
         .then(function (response) {
             //iterates through the information
             for (art in response.data) {
+                console.log('----------------------------------------------------');
                 console.log(response.data[art].venue.name);
                 console.log(moment(response.data[art].datetime).format('MM/DD/YYYY'));
                 console.log(`${response.data[art].venue.city}, ${response.data[art].venue.region}, ${response.data[art].venue.country}`);
@@ -98,30 +107,33 @@ function concertThis() {
 }
 
 function doWhatItSays() {
-    fs.readFile('./random.txt', 'utf8', function (error, data) {
-        //if there's an error
-        if (error) {
-            console.log(`Error has occurred: ${error}`);
-        } else {
-            //splits the data into an array
-            var whatSays = data.split(', ');
-            //sets the liriCommand
-            liriCommand = whatSays[0];
-            //sets the liriChoice
-            liriChoice = whatSays.slice(1).join(' ');
-            //commands based on the liriCommand 
-            switch (liriCommand) {
-                case 'spotify-this-song':
-                    spotifyThisSong();
-                    break;
-                case 'movie-this':
-                    movieThis();
-                    break;
-                case 'concert-this':
-                    concertThis();
-                    break;
-            }
+    fs
+        .readFile('./random.txt', 'utf8', function (error, data) {
+            //if there's an error
+            if (error) {
+                console.log(`Error has occurred: ${error}`);
+            } else {
+                //splits the data into an array
+                var whatSays = data.split(', ');
+                //sets the liriCommand
+                liriCommand = whatSays[0];
+                //sets the liriChoice
+                liriChoice = whatSays
+                    .slice(1)
+                    .join(' ');
+                //commands based on the liriCommand
+                switch (liriCommand) {
+                    case 'spotify-this-song':
+                        spotifyThisSong();
+                        break;
+                    case 'movie-this':
+                        movieThis();
+                        break;
+                    case 'concert-this':
+                        concertThis();
+                        break;
+                }
 
-        }
-    })
+            }
+        })
 }
