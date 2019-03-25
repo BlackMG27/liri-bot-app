a88c531ecdfcb8935a0d28dc764d48840fa30197
BlackMG27 <oike279@gmail.com>
2019-03-25 13:51:01 -0500

    added the default searches for spotifyThisSong and movieThis functions thus completing MVP

--- keys.js
+++ keys.js
@@ -4,5 +4,3 @@ exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
-
-exports.bandsInTown = process.env.BANDSINTOWN_ID;
--- liri.js
+++ liri.js
@@ -30,33 +30,42 @@ switch (liriCommand) {
 }
 //spotify function
 function spotifyThisSong() {
+    //checks to see if the user entered something
+    if (!process.argv[3]) {
+        //sets liriChoice to default
+        liriChoice = 'The Sign';
+    }
+    console.log(liriChoice);
+    console.log(`Someting shoul be here`);
+
     //searches spotify
-    spotify
-        .search({
-            type: 'track',
-            query: liriChoice
-        }, function (err, data) {
-            //catches the error
-            if (err) {
-                return console.log('Error occurred: ' + err);
-            }
-            //gets the specific data
+    spotify.search({
+        type: 'track',
+        query: liriChoice
+    }, function (err, data) {
+        //catches the error
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        // gets the specific artist and album data as it iterates through the
+        // data.tracks.items array
+        for (song in data.tracks.items) {
+            console.log('----------------------------------------------------------');
+            console.log(`Artist Name: ${data.tracks.items[song].album.artists[0].name}`);
+            console.log(`Album Name: ${data.tracks.items[song].album.name}`);
+            console.log(`Song Name: ${data.tracks.items[song].name}`);
+            console.log(`Album Release Date: ${moment(data.tracks.items[song].album.release_date).format('MM/DD/YYYY')}`);
+            console.log(`Song Preview: ${data.tracks.items[song].preview_url}`);
+            console.log('----------------------------------------------------------');
+        }
 
-            //console.log(data.tracks.items[0]);
-            for (song in data.tracks.items) {
-                console.log('----------------------------------------------------------');
-                console.log(`Artist Name: ${data.tracks.items[song].album.artists[0].name}`);
-                console.log(`Album Name: ${data.tracks.items[song].album.name}`);
-                console.log(`Song Name: ${data.tracks.items[song].name}`);
-                console.log(`Album Release Date: ${moment(data.tracks.items[song].album.release_date).format('MM/DD/YYYY')}`);
-                console.log(`Song Preview: ${data.tracks.items[song].preview_url}`);
-                console.log('----------------------------------------------------------');
-            }
-
-        });
+    });
 }
 
 function movieThis() {
+    if (!process.argv[3]) {
+        liriChoice = `Mr. Nobody`;
+    }
     //formats liriChoice
     liriChoice = liriChoice.replace(/ /g, '+');
     //puts liriChoice in the movie URL
@@ -66,15 +75,17 @@ function movieThis() {
         .get(movieURL)
         .then(function (response) {
             //gets the movie info
+            console.log('----------------------------------------------------------');
             console.log(`Movie Title: ${response.data.Title}`);
             console.log(`Year Released: ${response.data.Year}`);
             console.log(`Movie Director(s): ${response.data.Director}`)
             console.log(`IMDB Rating: ${response.data.Ratings[0].Value}`);
             console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
-            console.log(`Movie Language: ${response.data.Language}`);
+            console.log(`Movie Language(s): ${response.data.Language}`);
             console.log(`Movie Actor(s): ${response.data.Actors}`);
             console.log(`Movie Genre(s): ${response.data.Genre}`);
             console.log(`Movie Plot: ${response.data.Plot}`);
+            console.log('----------------------------------------------------------');
         })
         .catch(function (error) {
             console.log(`An error occurred: ${error}`);
@@ -82,13 +93,11 @@ function movieThis() {
 }
 
 function concertThis() {
-    //console.log(liriChoice);
-    //gets rid of the white spaces
+    //console.log(liriChoice); gets rid of the white spaces
     liriChoice = liriChoice.replace(/ /g, '%20');
     //sets up the api call
     var concertURL = `https://rest.bandsintown.com/artists/${liriChoice}/events?app_id=e0fa108c-68a4-472d-90fc-7da6bc210785`;
-    //console.log(concertURL);
-    //gets the ajax calls
+    //console.log(concertURL); gets the ajax calls
     axios.get(concertURL)
         //smacks the api and spits out the information
         .then(function (response) {
